828d66f95a46d1864581168f03829ef09a8a9a30
5alidz <kzack-dev@hotmail.com>
2025-07-06 15:04:31 +0300

    minor fix

--- app/routes/projects/[project_id]/files/[encoded]/page.tsx
+++ app/routes/projects/[project_id]/files/[encoded]/page.tsx
@@ -53,20 +53,13 @@ export default function Page({ loaderData }: Route.ComponentProps) {
 
   return (
     <div className='overflow-hidden h-full w-full max-w-full max-h-full flex items-stretch'>
-      <div className='w-full h-full overflow-hidden flex items-stretch max-w-5xl'>
-        {isClient && (
-          <Editor key={filePath} content={content} fileName={filePath} />
-        )}
-      </div>
-      <div className='w-full p-8 grid content-between h-[100vh] overflow-y-auto overflow-x-hidden max-w-xl relative'>
+      <div className='w-full p-8 grid content-between h-[100vh] overflow-y-auto overflow-x-hidden max-w-md relative'>
         <div className='h-[calc(100dvh-4rem)] flex items-center justify-center'>
           <div className='flex gap-3 font-mono items-center bg-background-2 p-1 rounded-2xl border border-border pr-4'>
             <div className='p-2 rounded-xl bg-background-1/50'>
               <MdInfoOutline className='w-5 h-5 text-amber-600' />
             </div>
-            <span className='text-sm'>
-              File threads workflow is coming soon.
-            </span>
+            <span className='text-sm'>Visual workflow is coming soon.</span>
           </div>
         </div>
         <div className='pointer-events-none opacity-30 absolute bottom-8 inset-x-8'>
@@ -76,6 +69,11 @@ export default function Page({ loaderData }: Route.ComponentProps) {
             placeholder='Start a thread about this file...'
           />
         </div>
+      </div>
+      <div className='w-full h-full overflow-hidden flex items-stretch border border-border'>
+        {isClient && (
+          <Editor key={filePath} content={content} fileName={filePath} />
+        )}
       </div>
     </div>
   );
